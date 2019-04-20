3e20790ab77788857e9270fedc916e8e2a56f70f
lycHub <1120805364@qq.com>
2019-04-20 17:12:12 +0800

    sw

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -35,9 +35,19 @@ module.exports = merge(baseConfig, {
           WorkboxPlugin.GenerateSW({}) 可以直接生成 service-worker 文件
         */
       new WorkboxPlugin.GenerateSW({
+        cacheId: 'webpack-pwa', // 设置前缀
         skipWaiting: true, // 强制等待中的 Service Worker 被激活
         clientsClaim: true, // Service Worker 被激活后使其立即获得页面控制权
-       
+        swDest: 'service-wroker.js', // 输出 Service worker 文件
+        globPatterns: ['**/*.{html,js,css,png.jpg}'], // 匹配的文件
+        globIgnores: ['service-wroker.js'], // 忽略的文件
+        runtimeCaching: [
+            // 配置路由请求缓存
+            {
+                urlPattern: /.*\.js/, // 匹配文件
+                handler: 'networkFirst' // 网络优先
+            }
+        ]
     })
   ],
   optimization: {
