d973650d535629dceab9d8f3b98b14c2090ee7f8
lycHub <1120805364@qq.com>
2019-04-22 19:05:10 +0800

    end

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,17 @@
 const { resolve } = require('path');
+const CopyRightPlugin = require('./plugins/copy-right.plugin');
 
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
-  resolveLoader: {
-    // loader查找顺序，从左到右
-    modules: ['node_modules', './loaders/']
-  },
   module: {
    rules: [
-     {
-      test: /\.js$/,
-      use: [
-        'replaceLoader',
-        {
-          loader: 'replaceLoaderAsync',
-          options: {
-            name: 'MadaoX'
-          }
-        },
-      ]
-     }
-   ]   
-  }
+     
+   ]
+  },
+  plugins: [
+    new CopyRightPlugin({
+      name: 'Madao'
+    })
+  ]
 }
